8618a123f1a580558ac48683aecc137e8a2bd00a
Purity Wambua <puritywamb@gmail.com>
2024-07-12 10:39:43 +0300

    javascript event list

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert());
 }
